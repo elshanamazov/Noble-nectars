56ad969c85be53d3fa3986c08c2bee3260d5ef16
Elshan Namazov <elshanamazov@gmail.com>
2023-08-26 16:48:54 +0300

    update gulpfile.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,25 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const swiper = new Swiper('.swiper', {
-    loop: true,
-    centeredSlides: true,
-    slidesPerView: 1.4,
-    spaceBetween: 30,
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: false,
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1.2,
-        spaceBetween: 20,
-        centeredSlides: false,
-      },
-      576: {
-        slidesPerView: 1.4,
-        spaceBetween: 30,
-        centeredSlides: true,
-        initialSLide: 1,
-      },
-    },
-  });
+	const swiper = new Swiper('.swiper', {
+		loop: true,
+		centeredSlides: true,
+		slidesPerView: 1.4,
+		spaceBetween: 30,
+		autoplay: {
+			delay: 2000,
+			disableOnInteraction: false,
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1.2,
+				spaceBetween: 20,
+				centeredSlides: false,
+			},
+			576: {
+				slidesPerView: 1.4,
+				spaceBetween: 30,
+				centeredSlides: true,
+				initialSLide: 1,
+			},
+		},
+	});
 });
